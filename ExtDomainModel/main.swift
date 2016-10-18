855f6a4710effec4eb108c404c0ccd7ec1d7fa76
Seung Lee <shl7@uw.edu>
2016-10-17 19:56:26 -0700

    Initial Commit

---
+++ ExtDomainModel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExtDomainModel
+//
+//  Created by iGuest on 10/17/16.
+//  Copyright © 2016 SeungLee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
